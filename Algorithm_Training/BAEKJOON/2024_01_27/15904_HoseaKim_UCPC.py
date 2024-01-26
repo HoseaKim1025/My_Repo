c4ec028e369da00cfda00fb492fb6ad13026f82b
HoseaKim1025 <hoseakim1025@gmail.com>
2024-01-26 17:52:33 +0900

    asd

---
+++ Algorithm_Training/BAEKJOON/2024_01_27/15904_HoseaKim_UCPC.py
@@ -0,0 +1,26 @@
+in_str = input()
+
+old_str = ''
+new_str = ''
+for i in in_str:
+    if i == 'U' or i == 'C' or i == 'P':
+        old_str += i
+if 'UCPC' in old_str:
+        print('I love UCPC')
+else:
+    pre = 0
+    for j in old_str:
+        if pre == 0 and j == 'U':
+            new_str += j
+            pre = j
+        elif j == 'C' and (pre == 'U' or pre == 'P'):
+            new_str += j
+            pre = j
+        elif j == 'P' and pre == 'C':
+            new_str += j
+            pre = j
+        pre = None
+    if 'UCPC' in new_str:
+        print('I love UCPC')
+    else:
+        print('I hate UCPC')
